444a797a4f089c446355028c7e9974fd62a16b0b
p22yxi <p22yxi@fairfieldprep.org>
2021-12-09 19:14:02 -0800

    hw_8-4-2

--- hw8-4-2.py
+++ hw8-4-2.py
@@ -2,36 +2,47 @@ from random import randint
 
 
 def rock_paper_scissors():
+    win = 0
+    lose = 0
+    tie = 0
     """Play rock paper scissors""" 
     computer = randint(0, 2) 
     while True: 
-        player = int(input("Enter 0 for rock, 1 for paper, and 2 for scissors: "))
-        if player > 2:
-            continue   
+        player = input("Enter 0 for rock, 1 for paper, and 2 for scissors: ")
+       
+        if int(player) > 2:
+            continue
         if player == computer:
             print("It's a tie!")
-            return "tie"
-        elif player == 0:
+            tie += 1
+            continue
+        elif int(player) == 0:
             if computer == 1:
                 print("You lose, paper covers rock.\n")
-                return "loss"
+                lose += 1 
             else:
                 print("You win, rock crushes scissors!\n")
-                return "win"
-        elif player == 1:
+                win += 1
+        elif int(player) == 1:
             if computer == 2:
                 print("You lose, scissors cuts paper.\n")
-                return "loss"
+                lose += 1
             else:
                 print("You win, paper covers rock!\n")
-                return "win"
-        elif player == 2:
+                win += 1
+        elif int(player) == 2:
             if computer == 0:
                 print("You lose, rock crushes scissors.\n")
-                return "loss"
+                lose += 1
             else:
                 print("You win, scissors cuts paper!\n")
-                return "win"
-
+                win += 1
+        anotherplay = input("Enter Y if you want to continue the game, and enter N if you want to end the game: ")
+        if anotherplay == "Y":
+            continue
+        if anotherplay == 'N':
+            print('You win {0} games, lose {1} games and tie {2} games.'.format(win, lose, tie))
+            break
+        
 
 rock_paper_scissors()
